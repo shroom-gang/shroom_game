68a202c31ac789a6edce3b553a489eaee5603c91
tadhg95 <oneilltadhg55@gmail.com>
2021-04-28 14:55:24 +0000

    generation????

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,13 +1,20 @@
 extends KinematicBody
 
-export var speed : float = 25
-export var accel : float = 10
-export var air_accel : float = 5
-export var gravity : float = 0.5
-export var terminal_velocity : float = 54
-export var jump : float = 25
+export var player = {
+	"name" : "none",
+	"traits" : ["SillyTrait","SillyTrait2"],
+	"BaseStats" : [1,1,1,1,1,10]
+}
 
-export var mouse_sens : float = 0.3
+
+export(float) var speed = 23 + (5 * player.get("BaseStats")[5])
+export(float) var accel = speed * 0.1
+export(float) var air_accel = speed * 0.05
+export(float) var gravity = 0.8
+export(float) var terminal_velocity = 54
+export(float) var jump = speed
+
+export(float) var mouse_sens = 0.3
 
 var velocity : Vector3
 var y_velocity : float
@@ -18,7 +25,7 @@ onready var cam = $cambase/spring/cam
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
@@ -48,14 +55,14 @@ func movement(delta):
 	
 	direction = direction.normalized()
 	
-	var bruh = accel if is_on_floor() else air_accel
+	var bruh = accel if is_on_floor() or is_on_wall() else air_accel
 	velocity = velocity.linear_interpolate(direction * speed, bruh * delta)
 	if is_on_floor():
 		y_velocity = -0.01
 	else:
 		y_velocity = clamp(y_velocity - gravity, -terminal_velocity, terminal_velocity)
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor() or is_on_wall():
 		y_velocity = jump
 	
 	velocity.y = y_velocity
